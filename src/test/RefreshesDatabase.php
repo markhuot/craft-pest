8bbf8dfa8c555faac3c111b5490f4d40a90437e0
Mark Huot <mark@markhuot.com>
2022-02-16 14:41:23 -0500

    adds refreshes database trait

---
+++ src/test/RefreshesDatabase.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace markhuot\craftpest\test;
+
+use Symfony\Component\Process\Process;
+use yii\db\Transaction;
+
+trait RefreshesDatabase {
+
+    /**
+     * @var bool
+     */
+    protected $projectConfigChecked = false;
+
+    /**
+     * @var Transaction
+     */
+    protected $transaction;
+
+    function refreshDatabase() {
+        if (!$this->projectConfigChecked && $this->isProjectConfigDirty()) {
+            $this->projectConfigApply();
+        }
+
+        $this->projectConfigChecked = true;
+    }
+
+    protected function isProjectConfigDirty() {
+        $process = new Process(['./craft', 'project-config/diff']);
+        $exitCode = $process->run();
+
+        return $exitCode !== 0;
+    }
+
+    protected function projectConfigApply() {
+        $process = new Process(['./craft', 'project-config/apply', '--force']);
+        $process->setTty(true);
+        $process->start();
+
+        foreach ($process as $type => $data) {
+            if ($type === $process::OUT) {
+                echo $data;
+            } else {
+                echo $data;
+            }
+        }
+    }
+
+    protected function beginTransaction() {
+        $this->transaction = $this->craft->db->beginTransaction('READ UNCOMMITTED');
+    }
+
+    protected function endTransaction() {
+        $this->transaction->rollBack();
+    }
+
+}
