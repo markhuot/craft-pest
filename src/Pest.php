8391a3997688d2ed377951913f919e3bdadc60e1
Mark Huot <mark@markhuot.com>
2022-04-04 15:04:45 -0400

    makes entries macroable

--- src/Pest.php
+++ src/Pest.php
@@ -3,7 +3,11 @@
 namespace markhuot\craftpest;
 
 use craft\base\Plugin;
+use craft\elements\Entry;
+use craft\events\DefineBehaviorsEvent;
+use markhuot\craftpest\behaviors\Macroable;
 use markhuot\craftpest\services\Http;
+use yii\base\Event;
 
 /**
  * @property Http $http
@@ -16,6 +20,12 @@ class Pest extends Plugin {
         $this->controllerNamespace = 'markhuot\\craftpest\\console';
 
         parent::init();
+
+        Event::on(Entry::class, Entry::EVENT_DEFINE_BEHAVIORS, function (DefineBehaviorsEvent $event) {
+            $event->behaviors = [
+                Macroable::class,
+            ];
+        });
     }
 
 }
