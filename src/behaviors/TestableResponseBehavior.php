fc988d37172c6ecc4cb6e9e6dc58331a7b544224
Mark Huot <mark@markhuot.com>
2022-10-01 16:00:04 -0400

    Update TestableResponseBehavior.php

--- src/behaviors/TestableResponseBehavior.php
+++ src/behaviors/TestableResponseBehavior.php
@@ -24,10 +24,14 @@ class TestableResponseBehavior extends Behavior {
     }
 
 
-    public function querySelector($selector) {
+    function querySelector(string $selector) {
         $html = $this->response->content;
         $crawler = new Crawler($html);
         return new NodeList($crawler->filter($selector));
+    }
+
+    function expectSelector(string $selector) {
+       return $this->querySelector($selector)->expect();
     }
 
     // public function __isset($key) {
