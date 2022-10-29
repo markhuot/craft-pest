506423608e0f8b439a4cae9a31b8bac2d426da9f
Mark Huota <mark@markhuot.com>
2022-10-29 15:17:10 +0000

    avoiding module config override

--- src/config/app.web.php
+++ src/config/app.web.php
@@ -4,13 +4,13 @@ use markhuot\craftpest\web\Application;
 
 return [
     'class' => Application::class,
-    'modules' => [
-        // 'debug' => [
-        //     'class' => 'yii\debug\Module',
-        //     'allowedIPs' => ['127.0.0.1', '::1'],
-        // ],
-    ],
-    'bootstrap' => [
-        // 'debug',
-    ],
+    // 'modules' => [
+    //     'debug' => [
+    //         'class' => 'yii\debug\Module',
+    //         'allowedIPs' => ['127.0.0.1', '::1'],
+    //     ],
+    // ],
+    // 'bootstrap' => [
+    //     'debug',
+    // ],
 ];
