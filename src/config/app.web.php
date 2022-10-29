75a38b0dce4b3c62e608f429462c51c9ceb39973
Mark Huota <mark@markhuot.com>
2022-10-29 02:15:28 +0000

    test

--- src/config/app.web.php
+++ src/config/app.web.php
@@ -5,12 +5,12 @@ use markhuot\craftpest\web\Application;
 return [
     'class' => Application::class,
     'modules' => [
-        'debug' => [
-            'class' => 'yii\debug\Module',
-            'allowedIPs' => ['127.0.0.1', '::1'],
-        ],
+        // 'debug' => [
+        //     'class' => 'yii\debug\Module',
+        //     'allowedIPs' => ['127.0.0.1', '::1'],
+        // ],
     ],
     'bootstrap' => [
-        'debug',
+        // 'debug',
     ],
 ];
