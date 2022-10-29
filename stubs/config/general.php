942dffe022876d3ad6b5b7c4f9256a2891ef6dbb
Mark Huota <mark@markhuot.com>
2022-10-29 13:13:29 +0000

    devmode

--- stubs/config/general.php
+++ stubs/config/general.php
@@ -2,6 +2,7 @@
 
 return [
     'omitScriptNameInUrls' => getenv('CRAFT_OMIT_SCRIPT_NAME_IN_URLS'),
+    'devMode' => getenv('CRAFT_DEV_MODE'),
     'aliases' => [
         '@templates' => getenv('CRAFT_TEMPLATES_PATH'),
     ],
