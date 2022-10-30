0d466dfa47aeb5fc9e4ceaca1d6c4cd8355ffbb8
Mark Huota <mark@markhuot.com>
2022-10-30 03:27:07 +0000

    comments

--- src/config/app.web.php
+++ src/config/app.web.php
@@ -4,6 +4,10 @@ use markhuot\craftpest\web\Application;
 
 return [
     'class' => Application::class,
+
+    // I dont want to force enable this here because there's a lot of logic in
+    // the craft\debug\Module::bootstrap() that handles some edge cases. All
+    // that custom logic is enabled with the `devMode` setting.
     // 'modules' => [
     //     'debug' => [
     //         'class' => 'yii\debug\Module',
