b8fa88afc2aeb19b91239295bf8ffdd740cab322
Mark Huota <mark@markhuot.com>
2022-11-03 12:24:02 +0000

    removing old config

--- stubs/config/app.php
+++ stubs/config/app.php
@@ -1,10 +1,4 @@
 <?php
 
 return [
-    'modules' => [
-        'pest-module-test' => \markhuot\craftpest\modules\test\Module::class,
-    ],
-    'bootstrap' => [
-        'pest-module-test',
-    ]
 ];
