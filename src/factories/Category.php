c278d64c75fbd4397d8fc852975eb575218ae36f
Nathaniel Hammond <nathanielhammond@gmail.com>
2022-11-08 14:11:49 +0000

    Fixed group/field layout error when using Category factory

--- src/factories/Category.php
+++ src/factories/Category.php
@@ -26,5 +26,18 @@ class Category extends Element {
             'groupId' => $group->id,
         ]);
     }
+    
+    /**
+     * @inheritdoc
+     */
+    protected function setAttributes($attributes, $element)
+    {
+        // Set `groupId` early on the element so that it does break when trying to retrieve the field layout
+        if (isset($attributes['groupId'])) {
+            $element->groupId = $attributes['groupId'];
+            unset($attributes['groupId']);
+        }
 
+        return parent::setAttributes($attributes, $element);
+    }
 }
