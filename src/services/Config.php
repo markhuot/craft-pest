602eeb62fee800f049b1be07845232071efc0607
Aaron Bushnell <aaron.bushnell@gmail.com>
2022-12-21 08:51:17 -0500

    Revise early return
    If we aren't dealing with `app.web` we can just return the config data—whether it's an array _or_ a fluent-based config (which extends `BaseConfig`)

--- src/services/Config.php
+++ src/services/Config.php
@@ -3,16 +3,15 @@
 namespace craft\services;
 
 use craft\config\BaseConfig;
-use markhuot\craftpest\web\Application;
 
 class Config extends \markhuot\craftpest\overrides\Config
 {
-    public function getConfigFromFile(string $filename): array
+    public function getConfigFromFile(string $filename): array|BaseConfig
     {
         $original = parent::getConfigFromFile($filename);
 
         if (!is_array($original) || $filename !== 'app.web') {
-            return collect($original)->toArray();
+            return $original;
         }
 
         return array_merge($original, require __DIR__ . '/../config/app.web.php');
