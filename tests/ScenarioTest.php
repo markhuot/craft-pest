d16ba0858a9a8a77bfc9c27377c3a786232352f7
Mark Huot <mark@markhuot.com>
2023-01-04 04:44:18 -0500

    cleanup

--- tests/ScenarioTest.php
+++ tests/ScenarioTest.php
@@ -1,16 +1,18 @@
 <?php
 
+use craft\fields\PlainText;
 use markhuot\craftpest\factories\Field;
 use markhuot\craftpest\factories\User;
 use markhuot\craftpest\factories\Section;
 use markhuot\craftpest\factories\Entry;
+use markhuot\craftpest\exceptions\ModelStoreException;
 
 it('fails on required fields', function () {
-    $field = Field::factory()->type(\craft\fields\PlainText::class)->required(true)->create();
+    $field = Field::factory()->type(PlainText::class)->required(true)->create();
     $user = User::factory()->create();
     $section = Section::factory()->fields($field)->create();
 
-    $this->expectException(\markhuot\craftpest\exceptions\ModelStoreException::class);
+    $this->expectException(ModelStoreException::class);
 
     $entry = Entry::factory()
         ->section($section)
