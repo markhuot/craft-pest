81d90aeda130ab20389bd2894ed4ad3f3356a489
markhuot <mark@markhuot.com>
2023-07-14 13:40:58 -0400

    removing unused stubs

--- stubs/config/general.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'omitScriptNameInUrls' => getenv('CRAFT_OMIT_SCRIPT_NAME_IN_URLS'),
-    'devMode' => getenv('CRAFT_DEV_MODE'),
-    'aliases' => [
-        '@templates' => getenv('CRAFT_TEMPLATES_PATH'),
-    ],
-];
