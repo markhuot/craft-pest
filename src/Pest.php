9afa098d7aa40bbdceb3b83173ac85a75d4be9ed
markhuot <mark@markhuot.com>
2024-09-30 16:48:52 -0400

    using action for initial stubs

--- src/Pest.php
+++ src/Pest.php
@@ -10,12 +10,14 @@ use craft\events\DefineBehaviorsEvent;
 use craft\events\PluginEvent;
 use craft\services\Fields;
 use craft\services\Plugins;
+use markhuot\craftpest\actions\CopyInitialStubs;
 use markhuot\craftpest\actions\RenderCompiledClasses;
 use markhuot\craftpest\behaviors\ExpectableBehavior;
 use markhuot\craftpest\behaviors\FieldTypeHintBehavior;
 use markhuot\craftpest\behaviors\TestableElementBehavior;
 use markhuot\craftpest\behaviors\TestableElementQueryBehavior;
 use yii\base\Event;
+use function markhuot\craftpest\helpers\base\service;
 
 /**
  * @method static self getInstance()
@@ -31,16 +33,7 @@ class Pest extends Plugin {
             Plugins::EVENT_AFTER_INSTALL_PLUGIN,
             function (PluginEvent $event) {
                 if (is_a($event->plugin, Pest::class)) {
-                    if (!is_dir(CRAFT_BASE_PATH . '/tests')) {
-                        mkdir(CRAFT_BASE_PATH . '/tests');
-                    }
-                    if (!file_exists(CRAFT_BASE_PATH . '/tests/Pest.php')) {
-                        copy(__DIR__ . '/../stubs/init/ExampleTest.php', CRAFT_BASE_PATH . '/tests/ExampleTest.php');
-                        copy(__DIR__ . '/../stubs/init/Pest.php', CRAFT_BASE_PATH . '/tests/Pest.php');
-                    }
-                    if (!file_exists(CRAFT_BASE_PATH . '/phpunit.xml')) {
-                        copy(__DIR__ . '/../stubs/init/phpunit.xml', CRAFT_BASE_PATH . '/phpunit.xml');
-                    }
+                    service(CopyInitialStubs::class)();
                 }
             }
         );
